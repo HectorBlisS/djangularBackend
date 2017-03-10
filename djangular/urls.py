0404575d854a28603c4ec7cbfdbcd1d4fbea49f3
BlisS <contacto@fixter.org>
2017-03-10 10:39:50 -0600

    router

--- djangular/urls.py
+++ djangular/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from scrumboard import urls as scrumUrls
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^scrumboard/',
     	include(scrumUrls)),
+    url(r'^$',
+		TemplateView.as_view(template_name="scrumboard/home.html")),
 ]
--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
-from .api import ListApi, CardApi
+# from django.conf.urls import url
+from .api import ListViewSet, CardViewSet
+from rest_framework.routers import DefaultRouter
 
 
-urlpatterns = [
-	url(r'^lists$',
-		ListApi.as_view()),
-	url(r'^cards$',
-		CardApi.as_view()),
-]
+router = DefaultRouter()
+router.register(r'lists', ListViewSet)
+router.register(r'cards', CardViewSet)
+
+urlpatterns = router.urls
